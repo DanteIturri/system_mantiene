9d4b86f3b8b8ca824b434208cac36935b38b4b5c
Dante Iturri <danteiturri@gmail.com>
2022-08-30 10:58:02 -0300

    review code

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -2,10 +2,9 @@
 
 
 @section('css')
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap css">
   <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css">
 @endsection
-
 @section('content')
   <div class="app-content content ">
     <div class="content-overlay"></div>
@@ -15,16 +14,11 @@
           <div class="breadcrumbs-top">
             <h5 class="content-header-title float-left pr-1 mb-0">Lista de Usuarios</h5>
             <div class="breadcrumb-wrapper d-none d-sm-block">
-
             </div>
           </div>
         </div>
       </div>
       <div class="content-body mt-5">
-        <!-- Description -->
-
-        <!--/ Description -->
-        <!-- CSS Classes -->
         <section id="css-classes" class="card">
           <div class="card-header">
             <h4 class="card-title">Usuarios</h4>
@@ -49,7 +43,6 @@
                       <th>Telefono</th>
                       <th>Rol</th>
                       <th>Mi imagen</th>
-
 
                       <th>Acciones</th>
                     </tr>
@@ -93,99 +86,15 @@
             </div>
           </div>
         </section>
-        <!--/ CSS Classes -->
-        <!-- HTML Markup -->
-
-        <!--/ HTML Markup -->
-
       </div>
     </div>
   </div>
-  {{-- <div class="container">
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="card">
-                    <div class="card-header">
-                        <div style="display: flex; justify-content: space-between; align-items: center;">
-
-                            <span id="card_title">
-                                {{ __('Clientes') }}
-                            </span>
-
-                             <div class="float-right">
-                                <a href="{{ route('clients.create') }}" class="btn btn-primary btn-sm float-right"  data-placement="left">
-                                  {{ __('Crear Nuevo') }}
-                                </a>
-                              </div>
-                        </div>
-                    </div>
-                    @if ($message = Session::get('success'))
-                        <div class="alert alert-success">
-                            <p>{{ $message }}</p>
-                        </div>
-                    @endif
-
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table id="data_clients" class="table table-striped table-hover">
-                                <thead class="thead">
-                                    <tr>
-                                        <th>Id</th>
-                                        
-										<th>Rut</th>
-										<th>Nombre</th>
-										<th>Apellido</th>
-										<th>Email</th>
-										<th>Telefono</th>
-										<th>Razon Social</th>
-										<th>Datos Complementarios</th>
-										<th>Mi imagen</th>
-										
-
-                                        <th>Acciones</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($clients as $client)
-                                        <tr>
-                                            <td>{{ ++$i }}</td>
-                                            
-											<td>{{ $client->rut }}</td>
-											<td>{{ $client->name }}</td>
-											<td>{{ $client->last_name }}</td>
-											<td>{{ $client->email }}</td>
-											<td>{{ $client->phone_number }}</td>
-											<td>{{ $client->razon_social }}</td>
-											<td>{{ $client->complementary_data }}</td>
-											<td>{{ $client->profile_photo_path }}</td>
-
-                                            <td>
-                                                <form action="{{ route('clients.destroy',$client->id) }}" method="POST">
-                                                    <a class="btn btn-sm btn-primary " href="{{ route('clients.show',$client->id) }}"><i class="fa fa-fw fa-eye"></i> Mostrar datos</a>
-                                                    <a class="btn btn-sm btn-success" href="{{ route('clients.edit',$client->id) }}"><i class="fa fa-fw fa-edit"></i> Editar</a>
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-fw fa-trash"></i> Borrar</button>
-                                                </form>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-                {{-- {!! $clients->links() !!} --}}
 @section('js')
   <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
   <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
   <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap4.min.js"></script>
-
-
-
   <script>
     $('#data_clients').DataTable();
-
     ducument.getElementById("deleted").addEventListener("click", function(e) {
       event.preventDefault()
     });
